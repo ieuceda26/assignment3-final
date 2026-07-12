a0b2e831e39ce92b14d2e4c75dc5c76db683cb3a
ieuceda26 <ieuceda26@gmail.com>
2026-07-12 01:43:10 -0400

    added aboutviewcontroller and characterUI. Added the n ecessary properties in entity. Updated details page and was a success

--- src/main/java/com/example/assignment3/entity/Character.java
+++ src/main/java/com/example/assignment3/entity/Character.java
@@ -20,14 +20,27 @@ public class Character {
 
     private String role;
 
+    private String species;
+
+    private String imageUrl;
+
+    private String abilities;
+
+    private String appearance;
+
     // Constructors
     public Character() {}
 
-    public Character(String name, String description, String universe, String role) {
+    public Character(String name, String description, String universe, String role, String species, String imageUrl, String abilities, String appearance) {
         this.name = name;
         this.description = description;
         this.universe = universe;
         this.role = role;
+        this.species = species;
+        this.imageUrl = imageUrl;
+        this.abilities = abilities;
+        this.appearance = appearance;
+
     }
 
     // Getters
@@ -36,12 +49,19 @@ public class Character {
     public String getDescription() { return description; }
     public String getUniverse() { return universe; }
     public String getRole() { return role; }
-
+    public String getSpecies() { return species; }
+    public String getImageUrl() { return imageUrl; }
+    public String getAbilities() { return abilities; }
+    public String getAppearance() { return appearance; }
     // Setters
     public void setCharacterId(Long characterId) { this.characterId = characterId; }
     public void setName(String name) { this.name = name; }
     public void setDescription(String description) { this.description = description; }
     public void setUniverse(String universe) { this.universe = universe; }
     public void setRole(String role) { this.role = role; }
+    public void setSpecies(String species) { this.species = species; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
+    public void setAbilities(String abilities) { this.abilities = abilities; }
+    public void setAppearance(String appearance) { this.appearance = appearance; }
 }
 
